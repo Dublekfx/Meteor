9784515f8ddce0755cc3cb9cc9895df855e704f7
Dublekfx <dublekfx@gmail.com>
2013-03-27 16:08:53 -0400

    Added explosions, edited onCommand

--- src/com/github/dublekfx/Meteor/Meteor.java
+++ src/com/github/dublekfx/Meteor/Meteor.java
@@ -3,7 +3,6 @@ package com.github.dublekfx.Meteor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-//import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,35 +27,43 @@ public final class Meteor extends JavaPlugin {
 				if (args.length == 0)	{
 					m = new Meteorite(p.getTargetBlock(null, 128).getLocation());
 					m.genMeteorite();
+					m.setFalling(true);
+					m.dropMeteorite();
 					return true;
 				}
-				else if (args.length == 1)	{
-					m = new Meteorite(p.getTargetBlock(null, 128).getLocation(), Integer.parseInt(args[0]));
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("meteormod"))	{
+			if ((sender instanceof Player && sender.hasPermission("meteor.launch")) )	{
+				if (args.length == 1)	{
+					m = new Meteorite(Bukkit.getPlayer(args[0]).getLocation());
 					m.genMeteorite();
+					m.setFalling(true);
+					m.dropMeteorite();
 					return true;
 				}
 				else if (args.length == 2)	{
-					if (args[1].substring(0, 1).equalsIgnoreCase("p:"))	{
-						m = new Meteorite(Bukkit.getPlayer(args[1].substring(2, args[1].length())).getLocation(), Integer.parseInt(args[0]));
-						m.genMeteorite();
-						return true;
-					}
+					m = new Meteorite(Bukkit.getPlayer(args[0]).getLocation(), Integer.parseInt(args[1]));
+					m.genMeteorite();
+					m.setFalling(true);
+					m.dropMeteorite();
+					return true;
 				}
 			}
 			if (!(sender instanceof Player))	{
 				if (args.length == 1)	{
-					if (args[1].substring(0, 1).equalsIgnoreCase("p:"))	{
-						m = new Meteorite(Bukkit.getPlayer(args[1].substring(2, args[1].length())).getLocation());
-						m.genMeteorite();
-						return true;
-					}
+					m = new Meteorite(Bukkit.getPlayer(args[0]).getLocation());
+					m.genMeteorite();
+					m.setFalling(true);
+					m.dropMeteorite();
+					return true;
 				}
 				else if (args.length == 2)	{
-					if (args[1].substring(0, 1).equalsIgnoreCase("p:"))	{
-						m = new Meteorite(Bukkit.getPlayer(args[1].substring(2, args[1].length())).getLocation(), Integer.parseInt(args[0]));
-						m.genMeteorite();
-						return true;
-					}
+					m = new Meteorite(Bukkit.getPlayer(args[0]).getLocation(), Integer.parseInt(args[1]));
+					m.genMeteorite();
+					m.setFalling(true);
+					m.dropMeteorite();
+					return true;
 				}
 			}
 		}
--- src/com/github/dublekfx/Meteor/Meteorite.java
+++ src/com/github/dublekfx/Meteor/Meteorite.java
@@ -4,47 +4,72 @@ import java.util.ArrayList;
 import java.util.logging.Logger;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.EntityBlockFormEvent;
 
 public class Meteorite {
 	
 	private int radius;
 	private final int DEFAULT_RADIUS = 3;
 	private Location target;
+	private Location skyTarget;
 	private boolean falling;
+	private Material mat = Material.NETHERRACK;
 	private ArrayList<Location> sphereCoords = new ArrayList<Location>();
 
 	public Meteorite(Location xyz)	{
 		target = xyz;
 		radius = DEFAULT_RADIUS;
-		falling = true;
-		target.setY(255 - radius);
+		target = skyTarget.clone();
+		target.setY(target.getWorld().getHighestBlockAt(target).getY());
+		skyTarget.setY(255 - radius);
+		falling = false;
 	}
 	public Meteorite(Location xyz, int r)	{
 		target = xyz;
 		radius = r;
-		falling = true;
-		target.setY(255 - radius);
+		target = skyTarget.clone();
+		target.setY(target.getWorld().getHighestBlockAt(target).getY());
+		skyTarget.setY(255 - radius);
+		falling = false;
 	}
-	public Meteorite(Location xyz, int r, boolean drop)	{
-		target = xyz;
-		radius = r;		
-		falling = drop;
-		target.setY(255 - radius);
+	public boolean isFalling()	{
+		return falling;
+	}
+	public void setFalling(boolean drop)	{
+		drop = falling;
 	}
 		
-	public void genMeteorite()	{
-		this.genSphere();
-		for (Location a : sphereCoords)	{			
-				target.getWorld().spawnFallingBlock(a, Material.NETHERRACK, (byte) 0);
+	public void dropMeteorite()	{
+		if (sphereCoords.size() >= 1 && this.isFalling())	{	//Ensures that genSphereCoords has been run, and falling = true
+			for (Location a : sphereCoords)	{			
+				skyTarget.getWorld().spawnFallingBlock(a, mat, (byte) 0);
+			}
+			Logger.getLogger("Minecraft").info("Let Chaos Reign!");
 		}
-		Logger.getLogger("Minecraft").info("Let Chaos Reign!");
+		else
+			Logger.getLogger("Minecraft").info("Preconditions not met!");
 	}
 	
-	private void genSphere()	{
+	public void genMeteorite()	{	//Creates a floating ball. dropMeteor must be called separately
+		this.genSphereCoords();
+		for (Location a : sphereCoords)	{			
+				a.getBlock().setType(mat);
+		}
+	}
+	
+	@EventHandler
+	public void onEntityBlockForm(EntityBlockFormEvent event){
+		if(event.getBlock().getLocation().equals(target) && event.getBlock().getType().equals(mat))	{
+			this.explode();
+		}
+	}
+	
+	private void genSphereCoords()	{
 		double bpow = Math.pow(radius, 2);
-		double bx = target.getX();
-		double by = target.getY();
-		double bz = target.getZ();
+		double bx = skyTarget.getX();
+		double by = skyTarget.getY();
+		double bz = skyTarget.getZ();
 		
 		 for (int z = 0; z <= radius; z++) {
 	            double zpow = Math.pow(z, 2);
@@ -52,17 +77,21 @@ public class Meteorite {
 	                double xpow = Math.pow(x, 2);
 	                for (int y = 0; y <= radius; y++) {
 	                    if ((xpow + Math.pow(y, 2) + zpow) <= bpow) {
-	                    	sphereCoords.add(new Location(target.getWorld(), bx + x, by + y, bz + z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx + x, by + y, bz - z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx - x, by + y, bz + z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx - x, by + y, bz - z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx + x, by - y, bz + z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx + x, by - y, bz - z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx - x, by - y, bz + z));
-	                    	sphereCoords.add(new Location(target.getWorld(), bx - x, by - y, bz - z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx + x, by + y, bz + z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx + x, by + y, bz - z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx - x, by + y, bz + z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx - x, by + y, bz - z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx + x, by - y, bz + z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx + x, by - y, bz - z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx - x, by - y, bz + z));
+	                    	sphereCoords.add(new Location(skyTarget.getWorld(), bx - x, by - y, bz - z));
 	                    }
 	                }
 	            }
 		 }
+	}
+
+	private void explode()	{
+		target.getWorld().createExplosion(target, radius);
 	}
 }
